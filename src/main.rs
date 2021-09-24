afbb2efb7b2c809c026a97c2183b0d77af21cb27
rhysd <lin90162@yahoo.co.jp>
2021-09-24 14:23:38 +0900

    read from stdin when no argument is given

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,20 @@ use log::{debug, info};
 use rayon::prelude::*;
 use std::ffi::OsStr;
 use std::fs;
-use std::io::Write;
+use std::io;
+use std::io::{Read, Write};
 use std::path::PathBuf;
 use walkdir::WalkDir;
 
 struct Replacement(usize, usize, String);
 
-fn replace_all<W: Write>(mut out: W, input: &str, replacements: Vec<Replacement>) -> Result<()> {
+fn replace_all<W: Write>(
+    mut out: W,
+    input: &str,
+    replacements: impl Iterator<Item = Replacement>,
+) -> Result<()> {
     let mut i = 0;
-    for replacement in replacements.into_iter() {
+    for replacement in replacements {
         let Replacement(s, e, url) = replacement;
         out.write_all(input[i..s].as_bytes())?;
         out.write_all(url.as_bytes())?;
@@ -119,13 +124,9 @@ impl Redirector {
         Ok(red)
     }
 
-    fn redirect(&self, file: PathBuf) -> Result<()> {
-        info!("Fixing redirects in {:?}", &file);
-
-        let content = fs::read_to_string(&file)?;
-        let spans = UrlFinder::new().find_all(&content); // Collect to Vec to use par_iter which is more efficient than par_bridge
-        debug!("Found {} links in {:?}", spans.len(), &file);
-
+    fn find_and_replace<W: Write>(&self, out: W, content: &str) -> Result<usize> {
+        let spans = UrlFinder::new().find_all(content); // Collect to Vec to use par_iter which is more efficient than par_bridge
+        debug!("Found {} links", spans.len());
         let replacements = spans
             .into_par_iter()
             .filter_map(|(start, end)| match self.resolve(&content[start..end]) {
@@ -133,42 +134,63 @@ impl Redirector {
                 Err(e) => Some(Err(e)),
             })
             .collect::<Result<Vec<_>>>()?; // Collect to Vec to check errors before overwriting files
-
-        let out = fs::File::create(&file)?;
         let len = replacements.len();
-        replace_all(out, &content, replacements)?;
+        replace_all(out, &content, replacements.into_iter())?;
+        Ok(len)
+    }
 
-        info!("Fixed {} links in {:?}", len, &file);
+    fn fix_file(&self, file: PathBuf) -> Result<()> {
+        info!("Fixing redirects in {:?}", &file);
+
+        let content = fs::read_to_string(&file)?;
+        let out = fs::File::create(&file)?;
+        let count = self.find_and_replace(out, &content)?;
+
+        info!("Fixed {} links in {:?}", count, &file);
         Ok(())
     }
 
-    fn redirect_all<'a>(&self, paths: impl Iterator<Item = &'a OsStr> + Send) -> Result<()> {
+    fn fix_all_files<'a>(&self, paths: impl Iterator<Item = &'a OsStr> + Send) -> Result<()> {
         let count = paths
             .flat_map(WalkDir::new)
             .filter(|e| match e {
                 Ok(e) => e.metadata().map(|m| m.is_file()).unwrap_or(false),
                 Err(_) => true,
             })
-            .try_fold(0, |c, e| self.redirect(e?.into_path()).map(|_| c + 1))?;
+            .try_fold(0, |c, e| self.fix_file(e?.into_path()).map(|_| c + 1))?;
         info!("Processed {} files", count);
         Ok(())
+    }
+
+    fn fix<R: Read, W: Write>(&self, mut reader: R, writer: W) -> Result<usize> {
+        let mut content = String::new();
+        reader.read_to_string(&mut content)?;
+        let content = &content;
+        self.find_and_replace(writer, content)
     }
 }
 
 fn main() -> Result<()> {
     env_logger::init();
+
     let matches = App::new("fixred")
         .arg(
             Arg::new("PATH")
-                .about("Directory or file to fix")
+                .about("Directory or file path to fix")
                 .multiple_values(true),
         )
         .get_matches();
+
+    let red = Redirector::default();
     if let Some(paths) = matches.values_of_os("PATH") {
         debug!("Some paths are given via arguments");
-        let red = Redirector::default();
-        red.redirect_all(paths)
+        red.fix_all_files(paths)
     } else {
-        unimplemented!("TODO: read stdin and output result to stdout")
+        info!("Fixing redirects in stdin");
+        let stdin = io::stdin();
+        let stdout = io::stdout();
+        let count = red.fix(stdin.lock(), stdout.lock())?;
+        info!("Fixed {} links in stdin", count);
+        Ok(())
     }
 }
